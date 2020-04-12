f5e2b0acf76b0627a7bdc4503ec4eccb43872f26
haalto <aaltonen.hannes@gmail.com>
2020-04-12 18:11:58 +0300

    added one chart with called WB data of Finnish GDP

--- src/pages/ChartPage/ChartPage.tsx
+++ src/pages/ChartPage/ChartPage.tsx
@@ -1,32 +1,66 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import styled from 'styled-components'
-import { LineChart, Line } from 'recharts'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 import { getIndicatorByCountry } from '../../services/wbServices'
- 
+
+interface IDataObject {
+  value: number
+}
+
 const ChartPage: React.FC = () => {
 
-  //const [data, setData] = useState([])
+  const [data, setData] = useState<IDataObject[]>([])
   
-  const handleSubmit = async () => {
-    console.log('clicked')    
-    const data = await getIndicatorByCountry('BR', 'NY.GDP.MKTP.CD')
-    console.log(data)
+  const renderLineChart = (
+    <LineChart width={1200} height={400} data={data}>
+      <Line type="monotone" dataKey="value" stroke="#8884d8"/>
+      <CartesianGrid stroke="#ccc" />
+      <XAxis dataKey="date" />
+      <YAxis/>
+      <Tooltip/>
+    </LineChart>
+  ) 
+
+  const reduceValueSize = (data: IDataObject[]) => {
+    const reducedData: IDataObject[] = []
+    for (let item of data) {
+      if (item.value !== 0) {
+        item.value = item.value/1000000000        
+      }
+      reducedData.push(item)
+    }
+    return reducedData
+  }
+
+  const handleDataRequest = async (country: string = 'FI', indicator: string = 'NY.GDP.MKTP.CD') => {      
+    try {
+      const responseData = await getIndicatorByCountry(country, indicator)
+      const filteredData = responseData.filter((_, i) => i % 2 === 1)
+      const mergedData = filteredData.concat.apply([], [...filteredData]).reverse()
+      mergedData.pop()
+      const reducedData = reduceValueSize(mergedData)
+      setData(reducedData)
+    }
+    catch (e) {
+      console.log(e)
+    }  
   }
 
   return (    
     <div>
       <Navbar/>
       <MainWrapper>
-        Charts
-        <button onClick={handleSubmit}>TEST</button>
+        Finland GDP in billions $
+        <button onClick={()=>handleDataRequest()}>Render</button>
+        {renderLineChart}        
       </MainWrapper>
     </div>
   )
 }
 
 const MainWrapper = styled.div`
-  padding-top: 10vh;
+  padding: 10vw;
 `
 
 export default ChartPage
